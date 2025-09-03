4400e59d00d156d1bf0d315eda95b739cb0853c5
narasimhaDln <narasimha34327@gmail.com>
2025-09-03 21:56:14 +0530

    new project

--- employment-portal/job-listing/src/app/studio/[[...tool]]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import React from 'react';
-import { NextStudio } from 'next-sanity/studio';
-import { config } from '../../../../sanity.config';
-
-export default function StudioPage() {
-  return <NextStudio config={config} />;
-}
